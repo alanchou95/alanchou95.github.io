c7ab41236b8112f82dc7aa0d7550274e4ab7f676
Alan Chou <alanchou95@gmail.com>
2018-10-16 00:11:00 +0800

    Orb update
    - Add button click when enter/return key is pressed
    - Add validation of empty question
    - Replace input field value with placeholder

--- js/orb.js
+++ js/orb.js
@@ -1,4 +1,6 @@
 function response() {
+  var question = document.getElementById("question").value;
+
   var list = [
     "It is certain.",
     "It is decidedly so.",
@@ -21,14 +23,23 @@ function response() {
     "Outlook not so good.",
     "Very doubtful.",
   ];
-  var number = Math.floor(Math.random()*20);
-  document.getElementById('response').innerHTML = list[number];
+
+// VALIDATE THAT A QUESTION IS ENTERED
+
+  if (question == "") {
+    document.getElementById("response").innerHTML = "You need to ask a question.";
+  } else {
+    var number = Math.floor(Math.random()*20);
+    document.getElementById("response").innerHTML = list[number];
+  }
+
 }
 
-  var input = document.getElementById("myInput");
-  input.addEventListener("keyup", function(event) {
-    event.preventDefault();
+// CLICK "ASK" WHEN HIT ENTER KEY
+
+var input = document.getElementById("question");
+input.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
-      document.getElementById("myBtn").click();
+        document.getElementById("submit").click();
     }
-  });
+});
